2db1f9020110f1197484defc4f42d5ce32eba8c5
DinisCruz <dinis.cruz@owasp.org>
2012-04-24 08:39:21 +0100

    removed default use of Gaug.es

--- Web Applications/TM_Website/Javascript/TM/Settings.js
+++ Web Applications/TM_Website/Javascript/TM/Settings.js
@@ -4,6 +4,7 @@ TM.tmVersion = "TM 3.1";
 TM.tmWebServices     = '/Aspx_Pages/TM_WebServices.asmx/';
 TM.NotAuthorizedPage = '/Html_Pages/Gui/Panels/AD_Non_Authorized_User.html';
 
+//TM.Gui.GauguesID                                = null;     // set to GaugesID to enabled it
 TM.Gui.showLibraryStructureToAnonymous          = true;
 TM.Gui.LoadLibraryData      					= true
 TM.Gui.editMode			   						= false;
