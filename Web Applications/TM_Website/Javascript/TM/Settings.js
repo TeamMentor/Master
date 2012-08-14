23dc8cd58357a11d555493f730401a6a6847c51b
DinisCruz <dinis.cruz@owasp.org>
2012-08-14 11:08:51 +0100

    added fix for https://github.com/TeamMentor/Master/issues/33

--- Web Applications/TM_Website/Javascript/TM/Settings.js
+++ Web Applications/TM_Website/Javascript/TM/Settings.js
@@ -1,5 +1,5 @@
 //TM Settings
-TM.tmVersion = "TM 3.1";	
+TM.tmVersion = "TM 3.2 RC1";	
 
 TM.tmWebServices     = '/Aspx_Pages/TM_WebServices.asmx/';
 TM.NotAuthorizedPage = '/Html_Pages/Gui/Panels/AD_Non_Authorized_User.html';
@@ -16,7 +16,7 @@ TM.Debug.load_HomePage 						  	= true;
 TM.Debug.showExperimentalFeatures				= false;
 
 
-TM.Debug.show_DebugDiv						  	= false;	// show small box with links to open trace view
+TM.Debug.show_DebugDiv						  	= true;	// show small box with links to open trace view
 TM.Debug.show_TraceView							= false; 	// show call trace popup window
 TM.Debug.callTrace_LoadEnvironment 			  	= false;	// open traceview popup and setup call trace hooks
 TM.Debug.callTrace_LoadEnvironment_Before_Gui 	= false;
