4e9627c5b8946659f16089852c8e6d0ac05c89f1
DinisCruz <dinis.cruz@owasp.org>
2012-12-18 15:59:28 +0000

    Trying Nacny engine on TM_API

---
+++ Web Applications/TM_API/MainModule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+using Nancy.Routing;
+
+namespace TM_API
+{
+	public class MainModule : NancyModule
+	{
+		public MainModule(IRouteCacheProvider routeCacheProvider)
+		{
+			Get["/"]		= x => View["routes", routeCacheProvider.GetCache()];
+			Get["/test"]	= x => "Test";
+
+			Get["/razor-simple"] = x =>
+			{
+				var model = new  { FirstName = "Fraaaaaank" };
+				return View["razor-simple.cshtml", model];
+			};
+		}
+	}
+}
