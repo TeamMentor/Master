35658c4b75cfeba85a004457c97c3919b1e21b10
DinisCruz <dinis.cruz@owasp.org>
2013-01-12 06:03:08 +0000

    added ga.cs and a working version of Check_GoogleAnalitics UnitTest

--- Web Applications/TeamMentor.UnitTests/WebSite_Content/Test_CheckJavascriptResources.cs
+++ Web Applications/TeamMentor.UnitTests/WebSite_Content/Test_CheckJavascriptResources.cs
@@ -20,23 +20,28 @@ namespace TeamMentor.UnitTests.WebSite_Content
 		[Test]
 		public void Check_GoogleAnalitics()
 		{			
-			var assembly = this.type().Assembly;
+			var assembly		 = this.type().Assembly;
 
-			"CodeBase: {0}".info(assembly.CodeBase);
-			"Location: {0}".info(assembly.Location);
-
-			//this.script_Me().waitForClose();
-		}
-
-		/*Console.WriteLine("Executing Assembly: {0}".format(Assembly.GetExecutingAssembly()));
-			var assembly = Assembly.GetExecutingAssembly();			
-			"Location: {0}".info(assembly.Location);
-			"CodeBase: {0}".info(assembly.CodeBase);
-			"Escaped Evident: {0}".info(assembly.CodeBase);			
-
-			//var url = "http://www.google-analytics.com/ga.js";
-			//"Url:".info(url);
+			var dllLocation		 = assembly.CodeBase.subString(8);
+			var webApplications  = dllLocation.parentFolder()
+											  .pathCombine(@"\..\..\..");
+			var tmWebsite 		 = webApplications.pathCombine("TM_Website");
+			var gAnalyticsFolder = tmWebsite.pathCombine(@"Javascript\gAnalytics");
+			var gaFile 			 = gAnalyticsFolder.pathCombine("ga.js");
 			
-		}*/
+			Assert.That(dllLocation		.fileExists(), "dllLocation file");
+			Assert.That(webApplications .dirExists() , "webApplications dir");
+			Assert.That(tmWebsite		.dirExists() , "tmWebsite dir");
+			Assert.That(gAnalyticsFolder.dirExists() , "gAnalyticsFolder dir");
+			Assert.That(gaFile			.fileExists(), "gaFile file");
+
+			var tmVersion	  = gaFile.fileContents();
+			var googleVersion = "http://www.google-analytics.com/ga.js".GET();
+
+			Assert.That(tmVersion.valid()	, "ga.js tmVersion not valid");
+			Assert.That(googleVersion.valid(), "ga.js googleVerion not valid");
+
+			Assert.AreEqual(tmVersion, googleVersion, "ga.js files didn't match");
+		}		
 	}
 }
