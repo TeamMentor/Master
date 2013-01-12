82d8dc62f7cf1590646579f44004b7818bfd1096
DinisCruz <dinis.cruz@owasp.org>
2013-01-12 03:57:17 +0000

    Added Debug Logger, renamed unit test, added first NUnit Test (which will check Google's ga.js file)

---
+++ Web Applications/TeamMentor.UnitTests/WebSite_Content/Test_CheckJavascriptResources.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using NUnit.Framework;
+using O2.DotNetWrappers.ExtensionMethods;
+using O2.Kernel;
+using System.Diagnostics;
+using TeamMentor.CoreLib;
+
+namespace TeamMentor.UnitTests.WebSite_Content
+{
+	[TestFixture]
+	public class Test_CheckJavascriptResources
+	{
+		[SetUp]
+		public void setup()
+		{
+			PublicDI.log.writeToDebug();
+		}
+
+		[Test]
+		public void Check_GoogleAnalitics()
+		{
+			var assembly = Assembly.GetExecutingAssembly();			
+			"Location: {0}".info(assembly.Location);
+			"CodeBase: {0}".info(assembly.CodeBase);
+			"Escaped Evident: {0}".info(assembly.CodeBase);			
+
+			var url = "http://www.google-analytics.com/ga.js";
+			"Url:".info(url);
+			//this.script_Me().waitForClose();
+		}
+	}
+}
