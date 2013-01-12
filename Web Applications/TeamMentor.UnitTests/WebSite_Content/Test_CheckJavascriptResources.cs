6b5e98e59ba15698587babfbd82de0e652692550
DinisCruz <dinis.cruz@owasp.org>
2013-01-12 04:40:10 +0000

    Testing TeamCity Messaging

--- Web Applications/TeamMentor.UnitTests/WebSite_Content/Test_CheckJavascriptResources.cs
+++ Web Applications/TeamMentor.UnitTests/WebSite_Content/Test_CheckJavascriptResources.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using NUnit.Framework;
 using O2.DotNetWrappers.ExtensionMethods;
 using O2.Kernel;
@@ -19,6 +20,9 @@ namespace TeamMentor.UnitTests.WebSite_Content
 		[Test]
 		public void Check_GoogleAnalitics()
 		{
+			Console.WriteLine("##teamcity[message text='Console normal text' errorDetails='stack trace' status='NORMAL']");
+			"##teamcity[message text='Exception text' errorDetails='stack trace' status='ERROR']".info();
+			
 			var assembly = Assembly.GetExecutingAssembly();			
 			"Location: {0}".info(assembly.Location);
 			"CodeBase: {0}".info(assembly.CodeBase);
