fa69b0f4db64ca60629f4adc0ca33b1554247f34
DinisCruz <dinis.cruz@owasp.org>
2013-01-13 12:04:55 +0000

    New TMEvents class to allow TM Websites to receive callbacks on (for example) Global.asax.cs callbacks created class in TM_Website App_Config to be used on TM Instances specific customizations Added temp Application_BeginRequest test

---
+++ Web Applications/TM_Website/App_Code/TM_Customization.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using O2.DotNetWrappers.ExtensionMethods;
+using SecurityInnovation.TeamMentor.WebClient;
+using TeamMentor.CoreLib;
+using TeamMentor.CoreLib.WebServices;
+
+namespace TeamMentor.Website
+{
+    public class TM_Customizations
+    {
+	    public static void AppInitialize()
+	    {		 
+		    new TM_Customizations().CustomizeTMConfig();
+
+			TMEvents.OnApplication_BeginRequest.Add(
+				()=>{
+						if (HttpContextFactory.Request.Url.str().contains(".js", ".ashx",".asmx", ".jpg", ".gif").isFalse())
+							HttpContextFactory.Response.Write("<h1>Custom code</h1>");
+					});
+	    }
+
+	    public void CustomizeTMConfig()
+	    {
+		    var tmConfig = TMConfig.Current;
+			tmConfig.ShowContentToAnonymousUsers = true;
+	    }
+    }
+}
--- Web Applications/TeamMentor.CoreLib/TM_AppCode/Global.asax.cs
+++ Web Applications/TeamMentor.CoreLib/TM_AppCode/Global.asax.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Web;
+using O2.DotNetWrappers.ExtensionMethods;
 using SecurityInnovation.TeamMentor.WebClient.WebServices;
 using O2.Kernel;
 using System.Web.Routing;
@@ -9,35 +11,48 @@ using O2.Kernel.InterfacesBaseImpl;
 
 namespace TeamMentor.CoreLib
 {
+	public class TMEvents
+	{
+		public static List<Action> OnApplication_Error			{ get; set; }
+		public static List<Action> OnApplication_Start			{ get; set; }
+		public static List<Action> OnApplication_BeginRequest	{ get; set; }
+		
+		static TMEvents()
+		{
+			OnApplication_Error			= new List<Action>();
+			OnApplication_Start			= new List<Action>();
+			OnApplication_BeginRequest	= new List<Action>();			
+		}
+	}
+
 	public class Global : HttpApplication
 	{
 		protected void Application_Error(object sender, EventArgs e)
-		{			
+		{
+			TMEvents.OnApplication_Error.invoke();
 			var lastError = Server.GetLastError();
 			if (lastError is HttpException && (lastError as HttpException).GetHttpCode() == 404)
 			{				
 				new HandleUrlRequest().routeRequestUrl_for404();                                          
 			}						
 		}
-
 		 
 		protected void Application_Start				(object sender, EventArgs e)
 		{
-			PublicDI.log.LogRedirectionTarget = new MemoryLogger();
+			TMEvents.OnApplication_Start.invoke();
+			PublicDI.log.LogRedirectionTarget = new MemoryLogger();			
 			Admin_REST_Config.SetRouteTable();            
         }
-		protected void Session_Start					(object sender, EventArgs e)		{ }	
-
+		
 		protected void Application_BeginRequest			(object sender, EventArgs e)		
         {
+			TMEvents.OnApplication_BeginRequest.invoke();
 			UtilMethods.addDefaultRequestHeaders();
             new HandleUrlRequest().routeRequestUrl();                                  
         }
-        protected void Application_AcquireRequestState  (object sender, EventArgs e) // this doesn't work for non aspx requests
-        {            
-            //new HandleUrlRequest().routeRequestUrl();                                  
-        }
-        
+
+		protected void Session_Start					(object sender, EventArgs e) { }	
+        protected void Application_AcquireRequestState  (object sender, EventArgs e)		{}        
 		protected void Application_AuthenticateRequest	(object sender, EventArgs e)		{ }
 		protected void Session_End						(object sender, EventArgs e)		{ }
 		protected void Application_End					(object sender, EventArgs e)		{ }
