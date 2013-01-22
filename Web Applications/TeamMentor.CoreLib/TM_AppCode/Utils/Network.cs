b025f357ae8f3b43be8d53db279bc7a827ba690f
DinisCruz <dinis.cruz@owasp.org>
2013-01-22 16:03:18 +0000

    Re-adding HubSpot contact creation

---
+++ Web Applications/TeamMentor.CoreLib/TM_AppCode/Utils/Network.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TeamMentor.CoreLib.TM_AppCode.Utils
+{
+    public class Network
+    {
+        public static bool online()
+        {
+            //return O2.DotNetWrappers.Network.
+            return false;
+        }
+    }
+}
