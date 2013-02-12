a3db8b439df90538c67a8285da1b0c369bdff6f5
DinisCruz <dinis.cruz@owasp.org>
2013-02-12 13:58:38 +0000

    Added ability to send emails from TM Moved PBKDF2 code into Crypto.cs file

---
+++ Web Applications/TeamMentor.CoreLib/TM_AppCode/Utils/SendEmails.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net.Mail;
+using System.Net.Mime;
+using O2.DotNetWrappers.Network;
+
+namespace TeamMentor.CoreLib
+{
+    public class SendEmails
+    {        
+        public static string SmtpServer            { get; set; }
+        public static string SmtpServer_Username   { get; set; }
+        public static string SmtpServer_Password   { get; set; }
+        public static string From                  { get; set; }
+
+        static SendEmails()
+        {
+            SmtpServer          = "smtp.sendgrid.net";
+            SmtpServer_Username = "TeamMentor";
+            SmtpServer_Password = "";
+            From = "TM_Security_Dude@securityinnovation.com";
+        }
+
+        public bool send_TestEmail()
+        {
+            return send(From, "Test Email", "From TeamMentor");
+        }
+
+        public bool send(string to, string subject, string message, bool htmlMessage = false)
+        {
+            try
+            {
+                var stmpServerOnline = Mail.isMailServerOnline(SmtpServer);                
+                
+                var mailMsg = new MailMessage();
+
+                // To
+                mailMsg.To.Add(new MailAddress(to));
+                // From
+                mailMsg.From = new MailAddress(From);
+                // Subject and multipart/alternative Body
+                mailMsg.Subject = subject;                
+                mailMsg.AlternateViews.Add(AlternateView.CreateAlternateViewFromString(message, null, MediaTypeNames.Text.Plain));
+                if (htmlMessage)                                
+                    mailMsg.AlternateViews.Add(AlternateView.CreateAlternateViewFromString(message, null,MediaTypeNames.Text.Html));                
+                // Init SmtpClient and send
+                var smtpClient = new SmtpClient(SmtpServer, 587);
+                var credentials = new System.Net.NetworkCredential(SmtpServer_Username, SmtpServer_Password);
+                smtpClient.Credentials = credentials;
+
+                smtpClient.Send(mailMsg);                                
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Web Applications/TeamMentor.UnitTests/CoreLib/Test_Email.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Mail;
+using NUnit.Framework;
+using O2.DotNetWrappers.ExtensionMethods;
+using O2.DotNetWrappers.Network;
+using System.Net.Mime;
+using TeamMentor.CoreLib;
+
+namespace TeamMentor.UnitTests.CoreLib
+{
+    [TestFixture]
+    public class Test_Email
+    {        
+        public string to         = "TM_Security_Dude@securityinnovation.com";        
+        public string subject    = "TM Email Test";
+        public string message    = "This is a test email from TeamMentor";
+
+        [SetUp]
+        public void SetUp()
+        {
+            if(SendEmails.SmtpServer_Password.notValid())
+                Assert.Ignore("SmtpServer_Password is not set, so ignoring test");
+        }
+
+        [Test]
+        public void Send_Test_Email()
+        {                        
+            var stmpServerOnline = Mail.isMailServerOnline(SendEmails.SmtpServer);
+            Assert.IsTrue(stmpServerOnline);
+
+            var sendResult = new SendEmails().send(to, subject, message);
+            Assert.IsTrue(sendResult);
+        }
+
+    }
+}
