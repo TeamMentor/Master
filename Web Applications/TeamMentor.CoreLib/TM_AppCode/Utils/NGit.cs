7a0b0c626d8311852c4cc5935de6fb66d98fc07d
DinisCruz <dinis.cruz@owasp.org>
2013-02-17 01:02:17 +0000

    Added first NGit method to TeamMentor.CoreLib.dll

---
+++ Web Applications/TeamMentor.CoreLib/TM_AppCode/Utils/NGit.cs
@@ -0,0 +1,12 @@
+﻿using O2.FluentSharp;
+
+namespace TeamMentor.CoreLib.TM_AppCode.Utils
+{
+    public class NGit
+    {
+        public bool isRepository(string path)
+        {
+            return path.isGitRepository();
+        }
+    }
+}
