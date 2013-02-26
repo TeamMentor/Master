1443899e16bba537fad2544a6e20160dccd5f494
DinisCruz <dinis.cruz@owasp.org>
2013-02-26 13:23:15 +0000

    Small fix to UnitTest that failed in TeamCity

--- Web Applications/TeamMentor.UnitTests/_Test_ConfigAndHelpers/TM_XmlDatabase_InMemory.cs
+++ Web Applications/TeamMentor.UnitTests/_Test_ConfigAndHelpers/TM_XmlDatabase_InMemory.cs
@@ -40,26 +40,35 @@ namespace TeamMentor.UnitTests
         //Helpers                
         public void Install_LibraryFromZip_TopVulns()
         {
-            Install_LibraryFromZip("https://github.com/TeamMentor/Library_Top_Vulnerabilities/zipball/master");
+            Install_LibraryFromZip("https://github.com/TeamMentor/Library_Top_Vulnerabilities/zipball/master", "Top Vulnerabilities");
         }        
         public void Install_LibraryFromZip_OWASP()
         {
-            Install_LibraryFromZip("https://github.com/TeamMentor/OWASP_Library/zipball/master");
+            Install_LibraryFromZip("https://github.com/TeamMentor/OWASP_Library/zipball/master", "OWASP");
         }
         [Assert_Admin]
-        public void Install_LibraryFromZip(string pathToGitHubZipBall)
+        public void Install_LibraryFromZip(string pathToGitHubZipBall, string libraryName)
         {
-            tmXmlDatabase.Path_XmlLibraries = "_TempXmlLibraries".tempDir(false);                        
+            tmXmlDatabase.Path_XmlLibraries = "_TempXmlLibraries".tempDir(false);
+
+            if (tmXmlDatabase.tmLibrary(libraryName).notNull())
+            {
+                "[Install_LibraryFromZip] Skyping library {0} because it already existed".debug(libraryName);
+                return;
+            }
             var tmLibraries_Before = tmXmlDatabase.tmLibraries();            
 
             var result = tmXmlDatabase.xmlDB_Libraries_ImportFromZip(pathToGitHubZipBall, "");
             tmXmlDatabase.xmlDB_Load_GuidanceItems();  // extra step required to reload the guidance items
-
-            var tmLibraries_After = tmXmlDatabase.tmLibraries();            
             
-            Assert.IsTrue(result , "xmlDB_Libraries_ImportFromZip");                        
-            Assert.IsNotEmpty(tmLibraries_After, "Libraries should be there after install");
+            var tmLibraries_After = tmXmlDatabase.tmLibraries();
+            var installedLibrary  = tmXmlDatabase.tmLibrary(libraryName);
+            
+            Assert.IsTrue     (result                                             , "xmlDB_Libraries_ImportFromZip");                        
+            Assert.IsNotEmpty (tmLibraries_After                                  , "Libraries should be there after install");
             Assert.AreNotEqual(tmLibraries_After.size(), tmLibraries_Before.size(), "Libraries size should be different before and after");
+            Assert.IsNotNull  (installedLibrary                                   , "Could not find installed library: {0}".format(libraryName));
+            Assert.AreEqual   (installedLibrary.Caption, libraryName              , "After install library names didn't match");
         }
     }
 }
