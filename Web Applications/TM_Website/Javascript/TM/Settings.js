4f38f6372c974ae72890e5724b8ae6f34828ec60
Dinis Cruz <dinis.cruz@owasp.org>
2013-09-30 19:57:38 +0100

    Update Settings.js

--- Web Applications/TM_Website/Javascript/TM/Settings.js
+++ Web Applications/TM_Website/Javascript/TM/Settings.js
@@ -1,5 +1,5 @@
 //TM Settings
-window.TM.tmVersion            = "TM 3.4 - RC 4";
+window.TM.tmVersion            = "TM 3.4";
 window.TM.ArticleTitle         = "TeamMentor 3.4";
 
 window.TM.tmWebServices        = '/Aspx_Pages/TM_WebServices.asmx/';
