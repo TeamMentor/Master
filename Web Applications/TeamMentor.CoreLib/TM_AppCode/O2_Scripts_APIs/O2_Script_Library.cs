090dc5035fe563596472a8f2f061faaa506a6c04
Dinis Cruz <dinis.cruz@owasp.org>
2014-03-10 12:49:26 +0000

    Final couple fixes due to Nuget API changes

--- Web Applications/TeamMentor.CoreLib/TM_AppCode/O2_Scripts_APIs/O2_Script_Library.cs
+++ Web Applications/TeamMentor.CoreLib/TM_AppCode/O2_Scripts_APIs/O2_Script_Library.cs
@@ -21,18 +21,18 @@ namespace FluentSharp
         {
             return  @"
 return ""LogViewer"".popupWindow().add_LogViewer();
-//O2Ref:FluentSharp.BCL.dll";    
+//using FluentSharp.WinForms;
+//O2Ref:FluentSharp.WinForms.dll";    
         }
 
-        public static string script_REPL()
+/*        public static string script_REPL()
         {
             return @"
 HttpContext.Current.script_Me().waitForClose();
 return ""done"";
 //using System.Web;
 //O2Ref:FluentSharp.BCL.dll
-//O2Ref:FluentSharp.REPL.exe
-//O2Ref:O2_Platform_External_SharpDevelop.dll";
+//O2Ref:FluentSharp.REPL.exe";
         }
 
         public static string script_Database()
@@ -43,11 +43,10 @@ return ""done"";
 //using TeamMentor.CoreLib;
 //O2Ref:TeamMentor.Corelib.dll
 //O2Ref:FluentSharp.BCL.dll
-//O2Ref:FluentSharp.REPL.exe
-//O2Ref:O2_Platform_External_SharpDevelop.dll";
-        }
+//O2Ref:FluentSharp.REPL.exe";
+        }*/
         
-        public static string load_NGit_Dlls()
+/*        public static string load_NGit_Dlls()
         {
             return @"var a = typeof(Sharpen.URLEncoder);
 var b = typeof(NGit.Repository);
@@ -55,6 +54,6 @@ return a.notNull() && b.notNull();
 
 //O2Ref:NGit.dll
 //O2Ref:Sharpen.dll";
-        }
+        }*/
     }
 }
--- Web Applications/TeamMentor.UnitTests/TM_XmlDatabase/Test_Users_Management.cs
+++ Web Applications/TeamMentor.UnitTests/TM_XmlDatabase/Test_Users_Management.cs
@@ -57,8 +57,8 @@ namespace TeamMentor.UnitTests.TM_XmlDatabase
                 
                 Assert.NotNull  (passwordHash);
                 Assert.AreEqual (64, passwordHash.base64Decode_AsByteArray().size());
-                Assert.Less     (timeSpan.Seconds,2);             // slowest calculation should be faster than 2 seconds
-                Assert.Greater  (timeSpan.TotalMilliseconds, 50); // slowest calculation should be slower than 50 milliseconds
+                Assert.Less     (timeSpan.Seconds,2);                 // slowest calculation should be faster than 2 seconds
+                Assert.Greater  (timeSpan.TotalMilliseconds, i * 40); // slowest calculation should be slower than i* 15 (40, 80,120,160,200) milliseconds
             }
         }
         [Test] public void PBKDF2_Default()
