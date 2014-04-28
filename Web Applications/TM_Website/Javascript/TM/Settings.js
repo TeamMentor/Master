926e14904ebadaa9bb16790681b6d8f614412c7c
Dinis Cruz <dinis.cruz@owasp.org>
2014-04-28 12:58:12 +0100

    Final 3.4.1 version

--- Web Applications/TM_Website/Javascript/TM/Settings.js
+++ Web Applications/TM_Website/Javascript/TM/Settings.js
@@ -1,5 +1,5 @@
 //TM Settings
-window.TM.tmVersion            = "TM 3.4.1 - RC3";
+window.TM.tmVersion            = "TM 3.4.1";
 window.TM.ArticleTitle         = "TeamMentor 3.4";
 
 window.TM.tmWebServices        = '/Aspx_Pages/TM_WebServices.asmx/';
