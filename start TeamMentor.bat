ee77f2638f6b467c6779832192d8b210aef33899
Dinis Cruz <dinis.cruz@owasp.org>
2014-06-04 16:06:17 +0100

    Renamed 'TeamMentor' folder into 'Source_Code'

--- start TeamMentor.bat
+++ start TeamMentor.bat
@@ -1,5 +1,5 @@
 @echo off
 rem start "WebServer" "Tools/CassiniDev_4.0.exe" /port:12120 /portMode:Specific /path:"TeamMentor\TM_Website"
-start "WebServer" "Tools/CassiniDev_4.0.exe" /port:12120 /portMode:Specific /path:"publish"
+start "WebServer" "Tools/CassiniDev_4.0.exe" /port:12120 /portMode:Specific /path:"TeamMentor\Publish\Website.3.4"
 ping 123.45.67.89 -n 1 -w 3000 > nul
 start http://127.0.0.1:12120
