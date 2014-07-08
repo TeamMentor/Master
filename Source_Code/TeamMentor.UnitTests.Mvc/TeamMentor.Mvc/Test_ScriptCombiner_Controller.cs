e0d2e35beffa223074c5d6d7285418a2fa38d393
Dinis Cruz <dinis.cruz@owasp.org>
2014-07-08 16:41:11 +0100

    Making it 3.5 RC0 and minor changes

---
+++ Source_Code/TeamMentor.UnitTests.Mvc/TeamMentor.Mvc/Test_ScriptCombiner_Controller.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using TeamMentor.Mcv;
+
+namespace TeamMentor.UnitTests.TeamMentor.Mvc
+{
+    [TestFixture]
+    public class Test_ScriptCombiner_Controller
+    {
+        [Test]
+        public void Get()
+        {
+            var scriptCombiner = new ScriptCombiner_Controller();
+        }
+    }
+}
