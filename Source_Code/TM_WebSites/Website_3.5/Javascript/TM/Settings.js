a87d3da4cb4b300003916afafd8dcc390641d793
Dinis Cruz <dinis.cruz@owasp.org>
2014-07-22 07:20:25 +0100

    Making it version TM 3.5 - RC0.8 (with 382 passing UnitTests)

--- Source_Code/TM_WebSites/Website_3.5/Javascript/TM/Settings.js
+++ Source_Code/TM_WebSites/Website_3.5/Javascript/TM/Settings.js
@@ -1,5 +1,5 @@
 //TM Settings
-window.TM.tmVersion            = "TM 3.5 - RC0.1";
+window.TM.tmVersion            = "TM 3.5 - RC0.8";
 window.TM.ArticleTitle         = "TeamMentor 3.5";
 
 window.TM.tmWebServices        = '/Aspx_Pages/TM_WebServices.asmx/';
