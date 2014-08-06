9e914c112700d722b888cadafcf532e881a3a7ad
Dinis Cruz <dinis.cruz@owasp.org>
2014-08-06 16:18:39 +0100

    Fix for https://github.com/TeamMentor/Master/issues/833 and https://github.com/TeamMentor/Master/issues/870
    The #833 fix is a massive hack, and the code is very ugly. That said, it seems to work and there are very few differences between the 'html' and 'article' rendering (if there is one only minor prob with one article (and that is on the code block)
    
    The DoS solution is done by using a temp thread to run the transformation (which is given 2 secs to complete it)

--- Source_Code/TM_Backend/TeamMentor.Markdown/CreoleWiki_ExtensionMethods.cs
+++ Source_Code/TM_Backend/TeamMentor.Markdown/CreoleWiki_ExtensionMethods.cs
@@ -5,22 +5,105 @@ using System.Text;
 using System.Threading.Tasks;
 using Algorim.CreoleWiki;
 using FluentSharp.CoreLib;
+using FluentSharp.CoreLib.API;
 
 namespace TeamMentor.Markdown
 {
     public static class CreoleWiki_ExtensionMethods
     {
+        public static int MAX_RENDER_WAIT = 2000;
         public static string wikiText_Transform(this string wikiText)
-		{						
+		{		
+			var html = "";	
             try
-            {
-                return new CreoleParser().Parse(wikiText);  
+            {                
+                wikiText = wikiText.wikiCreole_Fix_WikiText_Bullets();
+                
+                var wikiParseThread = O2Thread.mtaThread(()=>
+                    {
+                         html = new CreoleParser().Parse(wikiText)              
+                                                  .wikiCreole_Replaced_Html_Code_Tag_with_Pre();                         
+                    });
+                if(wikiParseThread.Join(MAX_RENDER_WAIT).isFalse())
+                {
+                    "[CreoleWiki_ExtensionMethods][wikiText_Transform] failed for WikiText: \n\n{0}\n\n".error(wikiParseThread);
+                    wikiParseThread.Abort();
+                }
             }
             catch(Exception ex)
             {
-                ex.log("[wikiText_transform]");
-                return "";
+                ex.log("[wikiText_transform]");                
             }
+            return html;
 		}
+
+        /// <summary>
+        /// This is a hack to handle five scenarious that WikiCreole works differently from jsCreole:       
+        /// 
+        /// The fix is done using simple String replaces, since there are enough RegEx issues with WikiCreole already
+        /// </summary>
+        /// <param name="wikiText"></param>
+        /// <returns></returns>
+        public static string wikiCreole_Fix_WikiText_Bullets(this string wikiText)
+        {           
+            var lines = wikiText.split("\n");
+
+            for(var i = 0; i < lines.size() ; i++)                              
+            {
+                var line = lines[i];                    
+                if (line.starts("*" ).isTrue () &&                      // case one: * is on the beggining of the line 
+                    line.starts("**").isFalse() &&                      //             is not bold
+                    line.starts("* ").isFalse())                        //             is not followed by a space
+                { 
+                    var fix = "* " + line.removeFirstChar();
+                    wikiText = wikiText.replace(line, fix);
+                    continue;
+                }
+
+                if (line.starts("    *" ).isTrue () &&                   // case two: * is after 4 spaces
+                    line.starts("    **").isFalse() &&
+                    line.starts("    * ").isFalse())                    
+                { 
+                    var fix = "    * " + line.subString_After("    *");
+                    wikiText = wikiText.replace(line, fix);                    
+                    continue;
+                }
+                if (line.starts("#"  ).isTrue () &&                      // case three: #* is on the beggining of the line                     
+                    line.starts("# ").isFalse())                    
+                { 
+                    var fix = "# " + line.subString_After("#");
+                    wikiText = wikiText.replace(line, fix);                    
+                    continue;
+                }
+                if (line.starts("    #" ).isTrue () &&                   // case four: # is after 4 spaces                    
+                    line.starts("    # ").isFalse())                    
+                { 
+                    var fix = "    # " + line.subString_After("    #");
+                    wikiText = wikiText.replace(line, fix);                    
+                    continue;
+                }
+                if (line.starts("#*"  ).isTrue () &&                     // case five: #* is on the beggining of the line 
+                    line.starts("#**" ).isFalse() &&
+                    line.starts("#* " ).isFalse())                    
+                { 
+                    var fix = "#* " + line.subString_After("    #*");
+                    wikiText = wikiText.replace(line, fix);                    
+                    continue;
+                }
+                
+            }
+            return wikiText;
+            //return size2.join("\n");
+            var joinedLines = lines.join("".line());
+            var size = joinedLines.split_onLines().size();
+          //  return wikiText;
+            return joinedLines;
+        }
+
+        public static string wikiCreole_Replaced_Html_Code_Tag_with_Pre(this string htmlCode)
+        {
+            return htmlCode.replace("<code>","<pre>")
+                           .replace("</code>","</pre>");
+        }
     }
 }
