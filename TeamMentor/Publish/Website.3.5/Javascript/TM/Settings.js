4670014e224974729dfa846a4e9e4c0949e98166
Dinis Cruz <dinis.cruz@owasp.org>
2014-08-11 15:54:50 +0100

    Publish files for: TM 3.5 - RC5

--- TeamMentor/Publish/Website.3.5/Javascript/TM/Settings.js
+++ TeamMentor/Publish/Website.3.5/Javascript/TM/Settings.js
@@ -1,5 +1,5 @@
 //TM Settings
-window.TM.tmVersion            = "TM 3.5 - RC4";
+window.TM.tmVersion            = "TM 3.5 - RC5";
 window.TM.ArticleTitle         = "TeamMentor 3.5";
 
 window.TM.tmWebServices        = '/Aspx_Pages/TM_WebServices.asmx/';
