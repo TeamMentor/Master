6eb89306cf324d3e48291fc240657bc0e289ec0a
Michael <michaelfalllas@gmail.com>
2014-12-12 15:03:51 -0600

    Minor change to remote Resharper Annotation

--- Source_Code/TM_Backend/TeamMentor.Users/Users/TM_UserData_Ex_Users.cs
+++ Source_Code/TM_Backend/TeamMentor.Users/Users/TM_UserData_Ex_Users.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using FluentSharp.CoreLib;
-using JetBrains.Annotations;
 
 namespace TeamMentor.CoreLib
 {
